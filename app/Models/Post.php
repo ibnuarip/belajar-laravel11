33ad9562d02a02bd8080291ffe1eb79f8b2ae3f9
ibnuarip <ibnuarip1601@gmail.com>
2026-02-28 21:21:38 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,39 +1,11 @@
-<?php 
+<?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Support\Arr;
-
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Ibnu',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam commodi libero beatae accusamus mollitia natus pariatur odit quae quia error, provident quidem perferendis itaque saepe blanditiis corrupti, repudiandae ipsa! Maiores?'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Ibnu',
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad assumenda amet exercitationem est ipsa pariatur iste iure consectetur. Repellat voluptatum unde dolore dolor, quae asperiores earum in dolorem! Minus, porro?'
-        ]
-    ];
-    }
-
-    public static function find($slug): array
-    {
-      $post = Arr::first(static::all(), fn ($post) =>  $post['slug'] == $slug);
-
-      if(! $post) {
-        abort(404);
-      }
-
-      return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
